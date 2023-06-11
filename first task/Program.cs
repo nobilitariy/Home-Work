fee960245ca5708b1aa686c4f9afebe65fd8c6dc
artem_kopshik <artemktk@gmail.com>
2023-06-11 13:07:10 +0300

    Wrote the first task

--- first task/Program.cs
+++ first task/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿/*Напишите программу, которая на вход принимает два числа и выдаёт,
+какое число большее, а какое меньшее.
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3*/
+Console.Write("Enter two numbers: ");
+double numberA, numberB;
+numberA = Convert.ToDouble(Console.ReadLine());
+numberB = Convert.ToDouble(Console.ReadLine());
+if(numberA < numberB)
+{
+    Console.WriteLine($"max = {numberB}");
+}
+if(numberA > numberB)
+{
+    Console.WriteLine($"max = {numberA}");
+}
+if(numberA == numberB)
+{
+    Console.WriteLine("The numbers are equal");
+}
+
