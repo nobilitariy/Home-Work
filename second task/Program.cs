2b7403f5052e403f281f4e5b7e50146a0a8f55fe
artem_kopshik <artemktk@gmail.com>
2023-06-11 14:24:57 +0300

    Corrected second task

--- second task/Program.cs
+++ second task/Program.cs
@@ -3,20 +3,21 @@
 2, 3, 7 -> 7
 44 5 78 -> 78
 22 3 9 -> 22 */
-Console.WriteLine("Inter three numbers: ");
+Console.WriteLine("Inter three different numbers: ");
 double number1, number2, number3;
 number1 = Convert.ToDouble(Console.ReadLine());
 number2 = Convert.ToDouble(Console.ReadLine());
 number3 = Convert.ToDouble(Console.ReadLine());
-if(number1 > number2 && number1 > number3)
+if(number1 > number2 & number1 > number3)
 {
     Console.WriteLine(number1);
 }
-if(number2 > number1 && number2 > number3)
+if(number2 > number1 & number2 > number3) 
 {
     Console.WriteLine(number2);
 }
-if(number3 > number1 && number3 > number2)
+if(number3 > number1 & number3 > number2)
 {
     Console.WriteLine(number3);
 }
+
