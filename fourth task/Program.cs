dbee80d88cd1bc9b9e1c776b63859385383f28b4
nobilitariy <134000822+nobilitariy@users.noreply.github.com>
2023-06-13 08:29:12 +0000

    Task on android

--- fourth task/Program.cs
+++ fourth task/Program.cs
@@ -1,20 +1,21 @@
 Console.Write("Enter number: ");
 double number = Convert.ToDouble(Console.ReadLine());
+double i = 0;
 if(number > 0)
 {
-    for(i = 2, i <= number, i += 2)
+    for(i = 2; i <= number; i += 2)
     {
         Console.WriteLine(i);
     }
 }
 else if(number < 0)
 {
-    for(i = -2, i >= number, i -= 2)
+    for(i = -2; i >= number; i -= 2)
     {
         Console.WriteLine(i);
     }
 }
-esle 
+else
 {
     Console.WriteLine("Enter a number other than zero");
 }
