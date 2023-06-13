daf459253ecc24f264f8f14bcb97bddda7eed736
nobilitariy <134000822+nobilitariy@users.noreply.github.com>
2023-06-13 10:49:49 +0300

    Code written directly on github

--- fourth task/Program.cs
+++ fourth task/Program.cs
@@ -1,1 +1,21 @@
-﻿
+Console.Write("Enter number: ");
+double number = Convert.ToDouble(Console.ReadLine());
+if(number > 0)
+{
+    for(i = 2, i <= number, i += 2)
+    {
+        Console.WriteLine(i);
+    }
+}
+else if(number < 0)
+{
+    for(i = -2, i >= number, i -= 2)
+    {
+        Console.WriteLine(i);
+    }
+}
+esle 
+{
+    Console.WriteLine("Enter a number other than zero");
+}
+     
